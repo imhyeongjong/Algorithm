36077e6e0c4e812bb5d65fc0c7dedc00c917f002
임형종 <85678935+imhyeongjong@users.noreply.github.com>
2024-04-12 01:19:02 +0900

    [level 0] Title: 가위 바위 보, Time: 3.49 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] a = rsp.split("");
+        for(int i=0; i<rsp.length(); i++){
+            if(a[i].equals("2")){
+                answer+="0";
+            }else if(a[i].equals("0")){
+                answer+="5";
+            }else{
+                answer+="2";
+            }
+        }
+        return answer;
+    }
+}
